f8e1ef84a3c9b444275e4f9912ff05ef0e5c8a08
Alexandre <alexandre-henrick@outlook.com>
2023-10-01 11:35:46 -0300

    add prot exe

---
+++ prototype/exercicio_shapes_prot.py
@@ -0,0 +1,20 @@
+'''
+Escreva um código usando o Prototype onde precisamos renderizar diferentes tipos de formatos
+gráficos (Shape). Crie um classe que será base para os vários formatos (círculo, retângulo etc). Depois
+crie as classes concretas desses formatos e por fim o gerenciador dos protótipos
+'''
+
+class ShapePrototype:
+    pass
+
+class Circulo(ShapePrototype):
+    pass
+
+class Retangulo(ShapePrototype):
+    pass
+
+class Gerenciador:
+    pass
+
+if __name__=="__main__":
+    pass
